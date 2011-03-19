a69f137628b6a214a76f664f4f4c94e3284d7d3b
IamPersistent <develop@zestic.com>
2011-03-18 19:45:20 -0700

    create empty managers for each model

---
+++ Document/TagManager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sonata\NewsBundle\Document;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Sonata\NewsBundle\Model\TagInterface;
+use Sonata\NewsBundle\Model\TagManager as BaseTagManager;
+ 
+class TagManager extends BaseTagManager
+{
+
+}
